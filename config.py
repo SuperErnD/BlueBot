97c6438f44c15dd9f1d22f9f1b13811c1e46c582
SuperErnD <84639576+SuperErnD@users.noreply.github.com>
2022-02-16 22:17:38 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODc0OTU3NDY4NDcwNTAxMzg2.YROhpA.iqln7gfYfA_5PyXLNves3ZMT5og',
+    'token': 'ODc0OTU3NDY4NDcwNTAxMzg2.YROhpA.xZTN50J-hl4OvTiHNJymN8PV4Pw',
     'bot': 'BlueBot',
     'id': 874957468470501386,
     'prefix': '!',
