e88cb9642790982931f6ff7dff7c1ca4290cd1b0
SuperErnD <84639576+SuperErnD@users.noreply.github.com>
2022-02-26 19:53:07 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 settings = {
-    'token': 'ODc0OTU3NDY4NDcwNTAxMzg2.YROhpA.tWxXhks6gnPRmOQsI8D4WN3l0kQ',
-    'bot': 'BlueBot',
-    'id': 874957468470501386,
-    'prefix': 'b!',
-    'version': 'v2.2'
+    'token': os.getenv('TOKEN'),
+    'bot': os.getenv('BOT'),
+    'id': os.getenv('ID'),
+    'prefix': os.getenv('PREFIX'),
+    'version': os.getenv('VERSION')
 }
