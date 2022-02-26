cafa9aa75af69fe4e8bde6a84809089bb0c2f1b4
SuperErnD <84639576+SuperErnD@users.noreply.github.com>
2022-02-26 17:47:56 +0200

    Update lvls.py

--- lvls.py
+++ lvls.py
@@ -26,14 +26,14 @@ async def addxp(message, member: discord.Member):
 	#levelstable = [1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,11000,12000,13000,14000,15000,16000,17000,18000,19000,20000,21000,22000,23000,24000,25000,26000,27000,28000,29000,30000,31000,32000,33000,34000,35000,36000,37000,38000,39000,40000,41000,42000,43000,44000,45000,46000,47000,48,49,50,51,52,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97000,98000,99000]
 	
 	if member2['dolevel'] / 1000 >= 1: 
-		if member2['dolevel'] == 0:
+		if member2['dolevel'] >= 1000:
 			if member2['registred'] == 0:
 				xpcollection.update_one({
   				'user': a
 				},{
   					'$set': {
     				'lvls': member2['lvls'] + 1,
-    				'dolevel': member2['dolevel'] + 1,
+    				'dolevel': member2['dolevel'] - 1001,
 				'registred': member2['registred'] + 1
    				}
 				}, upsert=False)
@@ -45,7 +45,7 @@ async def addxp(message, member: discord.Member):
 			},{
   				'$set': {
     			'lvls': member2['lvls'] + 1,
-    			'dolevel': member2['dolevel'] + 1
+    			'dolevel': member2['dolevel'] - 1001
    			}
 			}, upsert=False)
 			print('Updated user id: ' + str(a) + '!')
