b2affe604db74424d52a958baf482e922711c3e1
SuperErnD <84639576+SuperErnD@users.noreply.github.com>
2022-02-26 19:25:25 +0200

    Add files via upload

---
+++ cryptfunc.py
@@ -0,0 +1,42 @@
+from cryptography.fernet import Fernet
+##def write_key():
+##    """
+##    Generates a key and save it into a file
+##    """
+##    key = Fernet.generate_key()
+##    with open("key.key", "wb") as key_file:
+##        key_file.write(key)
+##write_key()
+def load_key():
+    """
+    Loads the key from the current directory named `key.key`
+    """
+    return open("key.key", "rb").read()
+def encrypt(filename, key):
+    """
+    Given a filename (str) and key (bytes), it encrypts the file and write it
+    """
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        # read all file data
+        file_data = file.read()
+        # encrypt data
+        encrypted_data = f.encrypt(file_data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
+key = load_key()
+encrypt('config.py', key)
+def decrypt(filename, key):
+    """
+    Given a filename (str) and key (bytes), it decrypts the file and write it
+    """
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        # read the encrypted data
+        encrypted_data = file.read()
+    # decrypt data
+    decrypted_data = f.decrypt(encrypted_data)
+    # write the original file
+    with open(filename, "wb") as file:
+        file.write(decrypted_data)
+#decrypt('config.py', key)
