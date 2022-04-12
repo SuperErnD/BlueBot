f87a974ad6475c4d684e3e1c22473c7e89d75ae8
SuperErnD <84639576+SuperErnD@users.noreply.github.com>
2022-04-12 16:43:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ import asyncio
 import random
 import datetime
 import os
-import youtube_dl
+#import youtube_dl
 import json
 import lvls
 import motor.motor_asyncio
@@ -55,31 +55,31 @@ ytdl_format_options = {
 }
 ffmpeg_options = {"options": "-vn"}
 
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+#ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
+#class YTDLSource(discord.PCMVolumeTransformer):
+#    def __init__(self, source, *, data, volume=0.5):
+#        super().__init__(source, volume)
 
-        self.data = data
-
-        self.title = data.get("title")
-        self.url = data.get("url")
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(
-            None, lambda: ytdl.extract_info(url, download=not stream)
-        )
-
-        if "entries" in data:
-            # Takes the first item from a playlist
-            data = data["entries"][0]
-
-        filename = data["url"] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+#        self.data = data
+#
+#        self.title = data.get("title")
+#        self.url = data.get("url")
+#
+#    @classmethod
+#    async def from_url(cls, url, *, loop=None, stream=False):
+#        loop = loop or asyncio.get_event_loop()
+#        data = await loop.run_in_executor(
+#            None, lambda: ytdl.extract_info(url, download=not stream)
+#        )
+#
+#        if "entries" in data:
+#            # Takes the first item from a playlist
+#           data = data["entries"][0]
+#
+#        filename = data["url"] if stream else ytdl.prepare_filename(data)
+#        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)#
 
 intents = discord.Intents.all()
 intents.members = True
